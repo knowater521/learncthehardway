d73295f769ff3fd0b363c5b4ca98d777611e0059
shacker101 <qq1583362322@gmail.com>
2018-02-06 22:13:29 +0800

    增加练习22的内容

---
+++ ex22.h
@@ -0,0 +1,16 @@
+#ifndef _ex22_h
+#define _ex22_h
+
+// makes THE_SIZE in ex22.c available to other .c files
+extern int THE_SIZE;            //令THE_SIZE可以在其它的.c文件中被访问
+
+// gets and sets an internal static variable in ex22.c
+int get_age();
+void set_age(int age);
+
+// updates a static variable that's inside update_ratio
+double update_ratio(double ratio);
+
+void print_size();
+
+#endif
